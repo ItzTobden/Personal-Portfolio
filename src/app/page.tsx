284796f02eaa52efaca9dc00dba19e1ad0ea769d
ItzTobden <jigdreltsheringtobden@gmail.com>
2024-03-04 13:45:37 +0700

    project-structure-update-1

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function RootPage() {
+    return (
+        <div className='text-red-600'>
+            RootPage
+            <Button>
+                add
+            </Button>
+        </div>
+    )
+}
+
+export default RootPage
