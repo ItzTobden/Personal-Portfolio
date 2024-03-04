2c2a46f8af7d9dec1bc0b7ee052385846173462d
ItzTobden <jigdreltsheringtobden@gmail.com>
2024-03-04 21:47:04 +0700

    Perf-loading-update-1

--- src/components/ui/loading/loading-starter.tsx
+++ src/components/ui/loading/loading-starter.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+'use client'
 
-const StarterLoading = () => {
+import './loading.css'
+import Lottie from 'lottie-react'
+import pikachu from '../../../../public/lotties/Animation - pikachu.json'
+import React, { useEffect, useState } from 'react'
+
+const StarterLoading: React.FC = () => {
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+        if (progress < 100) {
+            setProgress(prevProgress => prevProgress + 1)
+        }
+        }, 40)
+
+        return () => clearInterval(interval)
+    }, [progress])
+
     return (
-        <div>StarterLoading</div>
+        <div className='loading-starter-container'>
+            <h1>Welcome to Drukpas Studio 🤗</h1>
+            <Lottie 
+                animationData={pikachu} 
+                className='w-44 h-44' 
+            />
+            <div className="loading-bar">
+                <div 
+                    className="progress-bar" 
+                    style={{width:`${progress}%`}}
+                ></div>
+            </div>
+        </div>
     )
 }
 
