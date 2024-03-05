7d217912abadd8d84a598741177652de78ad11bf
ItzTobden <jigdreltsheringtobden@gmail.com>
2024-03-05 21:35:57 +0700

    Perf: skills-update-1

--- src/components/body/Skills.tsx
+++ src/components/body/Skills.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import React, { useState } from 'react'
 import './body.css'
+import { SkillsData } from '@/data/skills-data'
 
 const Skills = () => {
     const [openSkills, setOpenSkills] = useState(false)
@@ -56,7 +57,43 @@ const Skills = () => {
                     onMouseMove={handleMouseMove}
                     onMouseUp={handleMouseUp}
                 >
-                    Skills Coming Soon 🫠
+                    <div className="skills-header">
+                        <Button
+                            variant={'link'}
+                            className='bg-transparent decoration-transparent p-0 m-0'
+                            onClick={toggleSkills}
+                        >
+                        <h1>❌</h1>
+                        </Button>
+                    </div>
+                    <div className="personal-desc">
+                        <h1>Name: Jigdrel Tshering Tobden</h1>
+                        <h1>Job: Junior Web Developer</h1>
+                        <h1>Country: Bhutan</h1>
+                        <h1>Level: 50</h1>
+                    </div>
+                    <div className="stats-wrapper">
+                        <h1>HP: 10185</h1>
+                        <h1>MP: 1250</h1>
+                    </div>
+                    <div className="personal-stats">
+                        <h1>Strength: 85</h1>
+                        <h1>Vitality: 60</h1>
+                        <h1>Agility: 90</h1>
+                        <h1>Intelligence: 85</h1>
+                        <h1>Sense: 70</h1>
+                    </div>
+                    <div className="skills-desc">
+                        <h1>[Skills]</h1>
+                        { SkillsData.map(item => (
+                            <div className="skills-list" key={item.id}>
+                                <h1>{item.name}</h1>
+                                <div className="skills-bar">
+                                    <div className="progress-bar" style={{ width: `${item.level}%` }} />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             )}
         </>
---
+++ src/data/skills-data.ts
@@ -0,0 +1,38 @@
+interface SkillsProps {
+    id: string,
+    name: string,
+    level: number
+}
+
+export const SkillsData = [
+    {
+        id: 1,
+        name: "HTML",
+        level: 90,
+    },
+    {
+        id: 2,
+        name: "CSS",
+        level: 90,
+    },
+    {
+        id: 3,
+        name: "Javascript",
+        level: 70,
+    },
+    {
+        id: 4,
+        name: "React",
+        level: 60,
+    },
+    {
+        id: 5,
+        name: "NextJS",
+        level: 80,
+    },
+    {
+        id: 6,
+        name: "Tailwind CSS",
+        level: 60,
+    }
+]
